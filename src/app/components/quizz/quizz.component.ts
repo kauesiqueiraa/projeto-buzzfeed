ee3dd010c073a9e3d9cdd9bb8cc51baf682083e5
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2024-01-25 10:45:33 -0300

    feat: created logic for quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answersSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
   }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex += 1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else {
+      const finalyAnswer:string = await this.checkResult(this.answers)
+
+      this.finished = true
+      this.answersSelected = quizz_questions.results[finalyAnswer as keyof typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previus, current, index, arr) => {
+      if(arr.filter(item => item === previus).length > arr.filter(item => item === current).length){
+        return previus
+      }else {
+        return current
+      }
+    })
+
+    return result
+  }
+
 
 }
